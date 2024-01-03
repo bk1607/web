d69553852f042b5d8658e4028af95351ab91f4ca
bk1607 <baluanumala16@gmail.com>
2024-01-03 15:17:48 +0530

    alb creation

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_lb" "main" {
+  name               = var.name
+  internal           = var.internal
+  load_balancer_type = var.load_balancer_type
+  #security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = var.subnets
+
+  enable_deletion_protection = var.enable_deletion_protection
+
+
+
+  tags = {
+    Environment = "production"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,5 @@
+variable "name" {}
+variable "internal" {}
+variable "load_balancer_type" {}
+variable "subnets" {}
+variable "enable_deletion_protection" {}
