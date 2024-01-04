7e10c8a6a09896551e36fc0aaa3c64bfaf6fc38c
bk1607 <baluanumala16@gmail.com>
2024-01-04 10:05:51 +0530

    modifying subnet variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "main" {
   internal           = var.internal
   load_balancer_type = var.load_balancer_type
   #security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [var.subnets]
+  subnets            = var.subnets
 
   enable_deletion_protection = var.enable_deletion_protection
 
